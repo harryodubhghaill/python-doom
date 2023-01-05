48c13f1ff2d0a0b2fe8064d732cbb0c27ca3fcfd
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-05 17:34:41 +0000

    add player mvmt controls

--- player.py
+++ player.py
@@ -9,7 +9,38 @@ class Player:
     self.angle = PLAYER_ANGLE
 
   def movement(self):
-    pass
+    sin_a = math.sin(self.angle)
+    cos_a = math.cos(self.angle)
+    dx, dy = 0, 0
+    speed = PLAYER_SPEED * self.game.delta_time
+    speed_sin = speed * sin_a
+    speed_cos = speed * cos_a
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_w]:
+      dx += speed_cos
+      dy += speed_sin
+    if keys[pg.K_s]:
+      dx += -speed_cos
+      dy += -speed_sin
+    if keys[pg.K_a]:
+      dx += speed_cos
+      dy += -speed_sin
+    if keys[pg.K_d]:
+      dx += -speed_cos
+      dy += speed_sin
+
+    self.x += dx
+    self.y += dy
+
+    # rotate left with 'k'
+    if keys[pg.K_k]:
+      self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+    # rotate right with 'l'
+    if keys[pg.K_l]:
+      self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+    self.angle %= math.tau
+
 
   def update(self):
     self.movement()
