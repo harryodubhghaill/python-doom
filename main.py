541682f60690ee768c20959e42fcbfa6ed4b7cea
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-05 16:17:13 +0000

    add update funtion to game class

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ class Game():
   def __init__(self):
     pg.init()
     self.screen = pg.display.set_mode(RES)
-    
+    self.clock = pg.time.Clock()
+
+  def new_game(self):
+    pass
+
+  def update(self):
+    pg.display.flip()
+    self.clock.tick(FPS)
+    pg.display.set_caption(f'{self.clock.get_fps() :1f}')
+
+  
