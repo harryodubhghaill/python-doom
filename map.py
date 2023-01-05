43ad26652aa8237e2f6adb1e566e9174a53aa596
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-05 16:40:07 +0000

    add map class with function to define map boundaries

--- map.py
+++ map.py
@@ -35,3 +35,18 @@ mini_map = [
   [3, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 3],
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
 ]
+
+class Map:
+  def __init__(self, game):
+    self.game = game
+    self.mini_map = mini_map
+    self.world_map = {}
+    self.get_map()
+
+  def get_map(self):
+    for j, row in enumerate(self.mini_map):
+      for i, value in enumerate(row):
+        if value:
+          self.world_map[(i, j)] = value
+
+  
