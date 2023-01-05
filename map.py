33541ef8adebb8b8f6a0b5d8f9b024a1859a6ac8
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-05 16:43:08 +0000

    add draw method for map

--- map.py
+++ map.py
@@ -49,4 +49,6 @@ class Map:
         if value:
           self.world_map[(i, j)] = value
 
-  
+  def draw(self):
+    [pg.draw.rect(self.game.screen, 'darkgray', (pos[0]*100, pos[1]*100, 100, 100), 2)
+    for pos in self.world_map]
