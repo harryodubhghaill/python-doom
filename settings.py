30902bef06ffa6d69ebf7a0e6e3ca4820df4bc88
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-05 16:50:29 +0000

    add player settings

--- settings.py
+++ settings.py
@@ -1,3 +1,9 @@
 #game settings
-RES = WIDTH, HEIGHT = 1280, 720
+RES = WIDTH, HEIGHT = 1600, 900
 FPS = 60
+
+# player settings
+PLAYER_POS = 1.5, 5
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
