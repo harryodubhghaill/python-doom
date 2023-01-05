595adc20041d6495fe736577d6dca948a9788137
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-05 16:55:59 +0000

    add position properties to player class

--- player.py
+++ player.py
@@ -7,3 +7,19 @@ class Player:
     self.game = game
     self.x, self.y = PLAYER_POS
     self.angle = PLAYER_ANGLE
+
+  def movement(self):
+    pass
+
+  def update(self):
+    self.movement()
+
+  @property
+  def pos(self):
+    return self.x, self.y
+
+  @property
+  def map_pos(self):
+    return int(self.x), int(self.y)
+
+  
