65add8e1b55653a7960ef60f197cf154b48529ff
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-06 18:32:26 +0000

    add object handler class

---
+++ object_handler.py
@@ -0,0 +1,8 @@
+from sprite_object import *
+
+class ObjectHandler:
+  def __init__(self, game):
+    self.game = game
+    self.sprite_list = []
+    self.static_sprite_path = 'resources/sprites/static_sprites/'
+    self.anim_sprite_path = 'resources/sprites/animated_sprites/'
