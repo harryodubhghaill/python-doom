0cad77032a1a91cd46937e4e4828cf8a9815e400
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-08 00:05:06 +0000

    fix texture typo and add more NPCs

--- npc.py
+++ npc.py
@@ -191,7 +191,7 @@ class SoldierNPC(NPC):
     super().__init__(game, path, pos, scale, shift, animation_time)
 
 class CacoDemonNPC(NPC):
-  def __init__(self, game, path='resources/sprites/npc/cac_demon/0.png', pos=(10.5, 6.5), scale=0.7, shift=0.27, animation_time=250):
+  def __init__(self, game, path='resources/sprites/npc/caco_demon/0.png', pos=(10.5, 6.5), scale=0.7, shift=0.27, animation_time=250):
     super().__init__(game, path, pos, scale, shift, animation_time)
     self.attack_dist = 1.0
     self.health = 150
